770df7418ad398da05a3b8f8c6460eeb98b92329
givemepower8 <givemepower8@gmail.com>
2023-07-31 22:49:50 -0700

    improvement

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# htmlCanvas
+# Road map
+
+## JavaScript
+
+[MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+[DOM](https://developer.mozilla.org/en-US/docs/Web/API/Document)
+
+## Canvas
+
+<https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas>
+
+<https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API>
+
+## Animation
--- practices/js/first.js
+++ practices/js/first.js
@@ -1,6 +1,9 @@
-var anna = { name: "Anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
-var dave = { name: "Dave", age: 5, luckyNumbers: [3, 9, 40] };
-var kate = { name: "Kate", age: 9, luckyNumbers: [1, 2, 3] };
+window.addEventListener("load", startup, false);
 
+function startup(){
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-//test
+    ctx.fillStyle = "green";
+    ctx.fillRect(10, 10, 150, 100);
+}
