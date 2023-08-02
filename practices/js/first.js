479b3fcdb91e80e20c521baa86b4b285ff256812
Robin Wang <StudioRobin8@gmail.com>
2023-08-02 15:41:51 -0700

    minor

--- practices/js/first.js
+++ practices/js/first.js
@@ -4,6 +4,9 @@ function startup(){
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "red";
     ctx.fillRect(10, 10, 150, 100);
+    var score = {};
+    score["John"] = 0;
+    var addscore = score["John"] +1;
 }
