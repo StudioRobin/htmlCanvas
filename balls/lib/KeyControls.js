f611af36b17e0455ae973e67b1bf9af5198aff7f
Wei Wang <givemepower8@gmail.com>
2023-08-08 12:35:12 -0700

    key events

---
+++ balls/lib/KeyControls.js
@@ -0,0 +1,55 @@
+class KeyControls {
+    constructor() {
+      this.keys = {};
+  
+      // Bind event handlers
+      document.addEventListener("keydown", e => {
+        if (["ArrowLeft","ArrowUp", "ArrowRight", "ArrowDown", "Space", "KeyA","KeyW","KeyD","KeyS"].indexOf(e.code) >= 0) {
+          e.preventDefault();
+        }
+        this.keys[e.code] = true;
+      }, false);
+  
+      document.addEventListener("keyup", e => {
+        this.keys[e.code] = false;
+      }, false);
+    }
+  
+    key(key, value) {
+      if (value !== undefined) {
+        this.keys[key] = value;
+      }
+      return this.keys[key];
+    }
+  
+    reset () {
+      for (let key in this.keys) {
+        this.keys[key] = false;
+      }
+    }
+  
+    // Handle key actions
+    get action() {
+      return this.keys["Space"];
+    }
+  
+    get x() {
+      if (this.keys["ArrowLeft"] || this.keys["KeyA"]) {
+        return -1;
+      }
+      if (this.keys["ArrowRight"] || this.keys["KeyD"]) {
+        return 1;
+      }
+      return 0;
+    }
+  
+    get y() {
+      if (this.keys["ArrowUp"] || this.keys["KeyW"]) {
+        return -1;
+      }
+      if (this.keys["ArrowDown"] || this.keys["KeyS"]) {
+        return 1;
+      }
+      return 0;
+    }
+  }
--- cheatsheets/JavaScript.md
+++ cheatsheets/JavaScript.md
@@ -1,3 +1,12 @@
 # JavaScript
 
 ## Array
+
+
+## key events
+
+[keyCode](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode) is not recommended. Consider [KeyboardEvent.key](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key) or [KeyboardEvent.code](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode) for new code.
+
+* [keydown](https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event) could fire many times when holding.
+* [keyup](https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event) only file once.
+* [keypress](https://developer.mozilla.org/en-US/docs/Web/API/Element/keypress_event) is not recommended.
