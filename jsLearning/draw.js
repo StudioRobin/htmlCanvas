a8c20cf8a482d95c90a530b2f7bb02d316567361
Robin Wang <StudioRobin8@gmail.com>
2023-08-15 21:05:00 -0700

    line

--- jsLearning/draw.js
+++ jsLearning/draw.js
@@ -13,4 +13,16 @@ let maxSize = 50;
 drawBall(context, 200, 200, size);
 for(var i = 0; i<maxSize; i++){
     size++;
+    drawBall(context, 100, 100, size);
 };
+let drawLine = function (ctx, x1, y1, x2, y2){
+  console.log("line drawn");
+  // Start a new Path
+ctx.beginPath();
+ctx.moveTo(x1, y1);
+ctx.lineTo(x2, y2);
+
+// Draw the Path
+ctx.stroke();
+}
+drawLine(context,1,1,100,100);
